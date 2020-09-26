3562c7d0c00ab1216d81e03f34d4018f0afff494
Carlos Rodrigues <carlosheadst@gmail.com>
2020-09-26 15:49:19 -0300

    Criando API que faz analise de sentimento do twitter e devolve as medias das polaridades

---
+++ analisador.py
@@ -0,0 +1,60 @@
+import tweepy # Biblioteca de acesso à API do Twitter
+import re # Biblioteca de Expressões Regulares
+import pandas as pd # Biblioteca de manipulação de dados
+from textblob import TextBlob as tb # Biblioteca de análise de sentimento
+import numpy as np # Biblioteca matemática
+
+class Analisador():
+  consumer_key = None
+  consumer_secret = None
+  access_token = None
+  access_token_secret = None
+  api = None
+
+  def __init__(self):
+    self.consumer_key = 'FTNaD5Z8onfViLEceLK2KSeaH'
+    self.consumer_secret = 'MYJiY4U6lASYp4q7qe2vWQpxCHUykdBUHmGdm7ybbeKpr3rY4g'
+    self.access_token = '1236454673822953477-RLxbfQDriec8LnnD9SuT99DreSkwBo'
+    self.access_token_secret = 'qasGdiFqZkh7UUTa5ysFNCsIgSmAMa6AoRnWnadBZAikp'
+
+    auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+    auth.set_access_token(self.access_token, self.access_token_secret)
+
+    self.api = tweepy.API(auth)
+
+  def analisar(self, nome_perfil_usuario):
+    list_tweets = self.obter_tweets(nome_perfil_usuario = nome_perfil_usuario, quantidade = 100)
+
+    analysis = None
+    numPos = 0
+    numNeg = 0
+    numNeu = 0
+    total = 0 
+
+    for tweet in list_tweets:
+      analysis = tb(tweet)
+      polarity = analysis.sentiment.polarity
+
+      total += 1
+      if polarity > 0:
+        numPos += 1
+      elif polarity < 0:
+        numNeg += 1
+      elif polarity == 0:
+        numNeu += 1
+
+    mediaPos = numPos/total
+    mediaNeg = numNeg/total
+    mediaNeu = numNeu/total
+
+    return { 'media_positiva': mediaPos, 'media_negativa': mediaNeg, 'media_neutra': mediaNeu }
+
+  def obter_tweets(self, nome_perfil_usuario, quantidade):
+    results = self.api.user_timeline(screen_name = nome_perfil_usuario, count = quantidade, tweet_mode = 'extended')
+    tweets = []
+
+    for r in results:
+      tweet = re.sub(r'http\S+', '', r.full_text)
+      tweets.append(tweet.replace('\n', ' '))
+
+    return tweets
---
+++ routes.py
@@ -0,0 +1,18 @@
+from analisador import Analisador
+from flask import Flask, request
+
+app = Flask(__name__) # Inicializa a aplicação
+
+@app.route('/analisar', methods=['POST'])
+def analisar():
+  body = request.get_json()
+
+  if ('nome_perfil_usuario' not in body):
+    return { 'Erro': 'É necessário informar um nome de perfil de usuário!' }
+
+  analisador = Analisador()
+
+  return analisador.analisar(body['nome_perfil_usuario'])
+
+if __name__ == '__main__':
+  app.run(debug=True) #executa a aplicação
